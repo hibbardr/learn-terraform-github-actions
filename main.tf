2f6766696ff2a774dd8c96f34a2ed44c152af7c2
hibbardr <59662877+hibbardr@users.noreply.github.com>
2023-04-14 16:50:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
   access_key = "ASIA35KEIHUNNNUXYUKN"
   secret_key = "+v1InWjEn4+zIzuLYA0ao+yiFgZVA0ZhPEvRz/iN"
   token      = "FwoGZXIvYXdzEB4aDICq/A84Wr97ECPjkCLJAZRYUFn7RymPFcvto/sr+pgl/bZBB7Es23/0B8xdRFUzq9aD4Q4/inzDSum2oc0HnE7zo+VobUO4WMhI4S6JdVSpItj+LHLdZvCeUc9Lm2jlPMwqxZ2bcpFuVBsh8wlF/FxulcwKhCcqg601GB3gNhnEj67b0l8kcJtkFl7MZJLw3mOHJDyturR6UGO+4y+fQ9ZChlAn79XfwkzBrgETsw4omk7K641aJYuu4Ar9kW0li5dL/ChBUHmwVz153ek+Vu2SOKCcX2SMiSiw5OahBjItw7X8QXRER6TyOZlrq7Uqzp7U9AO+ETPlXlRukzST2OeJa4Hed0RYHbDsLRSs"

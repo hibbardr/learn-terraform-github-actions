2c08e6e2ae59861bc605b2bb39f67aa3c8b1bf43
hibbardr <59662877+hibbardr@users.noreply.github.com>
2023-04-14 20:01:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.28.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-  required_version = ">= 0.14.0"
 
-  cloud {
-    organization = "Examples-2"
-
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
-}
 provider "aws" {
   region     = "us-east-1"
   access_key = "ASIA35KEIHUNNNUXYUKN"

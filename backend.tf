5c23e34d8dfddd260939b3523934a0d3ee345046
hibbardr <59662877+hibbardr@users.noreply.github.com>
2023-04-14 20:01:46 -0400

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.28.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 0.14.0"
+
+  cloud {
+    organization = "Examples-2"
+
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+}
